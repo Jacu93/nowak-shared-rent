b56273cff317fa5033fe5dd5e47e95c7c47c5716
piotriwaniuk <piotr.iwaniuk@ds-consultants.eu>
2019-01-23 08:55:43 +0100

    Initial commit

---
+++ src/main/java/com/piekoszek/nowaksharedrent/NowakSharedRentApplication.java
@@ -0,0 +1,14 @@
+package com.piekoszek.nowaksharedrent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NowakSharedRentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NowakSharedRentApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/piekoszek/nowaksharedrent/NowakSharedRentApplicationTests.java
@@ -0,0 +1,17 @@
+package com.piekoszek.nowaksharedrent;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NowakSharedRentApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
