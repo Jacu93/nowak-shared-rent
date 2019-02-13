7169d6d945b6b62fcf6c243b81c8c5d2b76282ed
Jacek Nowak <jackie.n93@gmail.com>
2019-02-13 12:38:16 +0100

    Fixing AuthControllerTest.groovy by adding new AuthService object creation. Removing usage of "where" along with "expect".

--- src/test/groovy/com/piekoszek/nowaksharedrent/auth/AuthControllerTest.groovy
+++ src/test/groovy/com/piekoszek/nowaksharedrent/auth/AuthControllerTest.groovy
@@ -1,44 +1,44 @@
 package com.piekoszek.nowaksharedrent.auth
 
 import spock.lang.Specification
+import spock.lang.Subject
 
 class AuthControllerTest extends Specification {
+
+    @Subject
+    AuthService authService
+
+    def setup() {
+        authService = new AccountConfiguration().authService()
+    }
 
     def "Save an account in database"() {
 
         given: "new account data is entered"
-        def acc = new Account("example@mail.com", "tester", "secret")
+        def account = new Account("example@mail.com", "tester", "secret")
 
         expect: "method createAccount should return true"
-        AuthService.createAccount(acc)
+        authService.createAccount(account)
     }
 
-    def "Saving 2 accounts in a row with different id (email)"(Account acc) {
+    def "Saving 2 accounts in a row with different id (email)"() {
 
         given: "data of the accounts that are going to be saved one after another"
         def firstAccount = new Account("example@mail.com", "tester", "secret")
         def secondAccount = new Account("testing@mail.com", "tester", "secret")
 
-        expect: "method createAccount should return true"
-        AuthService.createAccount(acc)
-
-        where: "true should be returned for both attempts"
-        acc | _
-        firstAccount | _
-        secondAccount | _
+        expect: "method createAccount should return true twice"
+        authService.createAccount(firstAccount)
+        authService.createAccount(secondAccount)
     }
 
-    def "Saving 2 accounts in a row with the same id (email)"(Account acc, boolean result) {
+    def "Saving 2 accounts in a row with the same id (email)"() {
 
         given: "data of the accounts that are going to be saved one after another"
         def account = new Account("example@mail.com", "tester", "secret")
 
-        expect: "method createAccount should return true"
-        AuthService.createAccount(acc) == result
-
-        where: "true should be returned only for the first attempt"
-        acc | result
-        account | true
-        account | false
+        expect: "method createAccount should return true only for the first attempt"
+        authService.createAccount(account)
+        authService.createAccount(account) == false
     }
 }
