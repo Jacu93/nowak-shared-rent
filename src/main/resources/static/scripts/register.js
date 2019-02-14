5272cf13c1360666e01a32158cb7b7e75edf698f
Jacek Nowak <jackie.n93@gmail.com>
2019-02-14 22:52:13 +0100

    adding brackets to if statements, scripts cleanup and adding fetch method to register.js

--- src/main/resources/static/scripts/register.js
+++ src/main/resources/static/scripts/register.js
@@ -3,10 +3,23 @@ function register() {
     var emailElement=document.getElementById("emailInput");
     var nameElement=document.getElementById("nameInput");
     var passwordElement=document.getElementById("passInput");
-    //var repasswordElement=document.getElementById("repassInput");
+    var repasswordElement=document.getElementById("repassInput");
 
     var url = 'http://localhost:8080/auth/signup';
-    var data = {"email": emailElement, "name": nameElement, "password": passwordElement};
+    var data = {"email": emailElement.value, "name": nameElement.value, "password": passwordElement.value};
+
+    if (passwordElement.value != repasswordElement.value){
+        document.getElementById("error").innerHTML = "Not the same passwords";
+        return;
+    }
+    if (!(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(emailElement.value))){
+        document.getElementById("error").innerHTML = "Wrong email address";
+        return;
+    }
+    if (!(passwordElement.value.length>3)){
+        document.getElementById("error").innerHTML = "Too short password";
+        return;
+    }
 
     fetch(url, {
       method: 'POST',
