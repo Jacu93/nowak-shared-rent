b728d4a6c9113780be501bd5b4c75cb478f396a0
Jacek Nowak <jackie.n93@gmail.com>
2019-02-25 18:44:20 +0100

    Feature/registration form (#4)
    * adding InMemoryAccountRepository and configuration (AccountConfiguration) for test purposes
    
    * adding groovy script for AuthController tests
    
    * rebuilding AuthControllerTest.groovy
    
    * Fixing AuthControllerTest.groovy by adding new AuthService object creation. Removing usage of "where" along with "expect".
    
    * Renaming AuthControllerTest -> AuthServiceTest
    
    * Removing NowakSharedRentApplicationTests
    
    * code cleanup and adding requested minor changes
    
    * adding register.js to handle registration process, adding additional field for password retyping
    
    * adding brackets to if statements, scripts cleanup and adding fetch method to register.js
    
    * registration confirmation presented to the user
    
    * registration confirmation presented to the user
    
    * adding groovy script for AuthController tests
    
    * rebuilding AuthControllerTest.groovy
    
    * Fixing AuthControllerTest.groovy by adding new AuthService object creation. Removing usage of "where" along with "expect".
    
    * Renaming AuthControllerTest -> AuthServiceTest
    
    * Removing NowakSharedRentApplicationTests
    
    * code cleanup and adding requested minor changes
    
    * adding register.js to handle registration process, adding additional field for password retyping
    
    * adding brackets to if statements, scripts cleanup and adding fetch method to register.js
    
    * registration confirmation presented to the user
    
    * registration confirmation presented to the user

---
+++ src/main/resources/static/scripts/register.js
@@ -0,0 +1,37 @@
+function register() {
+
+    var emailElement=document.getElementById("emailInput");
+    var nameElement=document.getElementById("nameInput");
+    var passwordElement=document.getElementById("passInput");
+    var repasswordElement=document.getElementById("repassInput");
+
+    var url = 'http://localhost:8080/auth/signup';
+    var data = {"email": emailElement.value, "name": nameElement.value, "password": passwordElement.value};
+
+    if (passwordElement.value != repasswordElement.value){
+        document.getElementById("error").innerHTML = "Not the same passwords";
+        return;
+    }
+    if (!(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(emailElement.value))){
+        document.getElementById("error").innerHTML = "Wrong email address";
+        return;
+    }
+    if (!(passwordElement.value.length>3)){
+        document.getElementById("error").innerHTML = "Too short password";
+        return;
+    }
+
+    fetch(url, {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers:{
+        'Content-Type': 'application/json'
+      }
+    }).then(res => res.json())
+    .then(response => console.log('Success:', JSON.stringify(response)))
+    .catch(error => console.error('Error:', error));
+
+    //ten blok ponizej mi nie dziala :( wywala cala funkcje register()
+    /*document.getElementById("error").innerHTML = "Account registered successfully!";
+    document.getElementById("error").style.color = #009933;*/
+}
