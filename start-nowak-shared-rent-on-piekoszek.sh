61a4d35592fbda351764ae380f42a3a2b0c4abf2
Jacek Nowak <jackie.n93@gmail.com>
2019-03-05 17:32:56 +0100

    adding .sh to the script names and changing script name as requested

--- start-nowak-shared-rent-on-piekoszek.sh
+++ start-nowak-shared-rent-on-piekoszek.sh
@@ -3,4 +3,4 @@
 mvn clean install
 cd ./target/
 scp nowak-shared-rent-0.0.1-SNAPSHOT.jar jackie@piekoszek.pl:/home/jackie/
-ssh jackie@piekoszek.pl 'bash /home/jackie/start-app-piekoszek'
+ssh jackie@piekoszek.pl 'bash /home/jackie/start-app-piekoszek.sh'
