7f2dd3cc7d800190f927e4d651df72d7e88119ae
Jacek Nowak <jackie.n93@gmail.com>
2019-05-23 12:06:17 +0200

    Added form validation for apartment creator

--- src/main/resources/static/scripts/administrationPage.js
+++ src/main/resources/static/scripts/administrationPage.js
@@ -3,37 +3,40 @@ window.onload = () => {
 }
 
 function newApartment() {
+    let form = document.getElementById("apartment-form");
 
-    let address = document.getElementById("address").value;
-    let city = document.getElementById("city").value;
-    let url = '/apartment'
-    let data = { "address": address, "city": city };
-    let headers = new Headers();
-    headers.append('Content-Type', 'application/json');
-    headers.append('Authorization', window.localStorage.getItem("accessToken"));
+    if (form.checkValidity() === true) {
+        let address = document.getElementById("address").value;
+        let city = document.getElementById("city").value;
+        let url = '/apartment'
+        let data = { "address": address, "city": city };
+        let headers = new Headers();
+        headers.append('Content-Type', 'application/json');
+        headers.append('Authorization', window.localStorage.getItem("accessToken"));
+        
+        fetch(url, {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: headers
+        })
+        .then(res => {
+            if (res.ok) {
+                res.json().then(json => {
+                    console.log('Success:', JSON.stringify(json));
+                    refreshToken();
+                    alert ("Apartment created!", "alert-success");
+                })
+            }
+            else {
+                res.json().then(json => console.log('Internal error:', JSON.stringify(json)))
+            }
+        })
+        .catch(error => console.error('Error:', error));
 
-    fetch(url, {
-        method: 'POST',
-        body: JSON.stringify(data),
-        headers: headers
-    })
-    .then(res => {
-        if (res.ok) {
-            res.json().then(json => {
-                console.log('Success:', JSON.stringify(json));
-                refreshToken();
-                alert ("Apartment created!", "alert-success");
-            })
-        }
-        else {
-            res.json().then(json => console.log('Internal error:', JSON.stringify(json)))
-        }
-    })
-    .catch(error => console.error('Error:', error));
-
-    setTimeout(function () {
-        $('.alert').alert('close')
-    }, 5000);
+        setTimeout(function () {
+            $('.alert').alert('close')
+        }, 5000);
+    }
 
     return false;
 }
