cb9529bd13b76ae5b37f0dbc2ed7431ca7379493
Jacek Nowak <jackie.n93@gmail.com>
2019-06-19 00:27:56 +0200

    Adding @Jwt and @AllArgsConstructor annotations for all controllers.

--- src/main/java/com/piekoszek/nowaksharedrent/apartment/ApartmentController.java
+++ src/main/java/com/piekoszek/nowaksharedrent/apartment/ApartmentController.java
@@ -2,27 +2,21 @@ package com.piekoszek.nowaksharedrent.apartment;
 
 import com.piekoszek.nowaksharedrent.jwt.Jwt;
 import com.piekoszek.nowaksharedrent.jwt.JwtData;
-import com.piekoszek.nowaksharedrent.jwt.JwtService;
 import com.piekoszek.nowaksharedrent.response.MessageResponse;
+import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@AllArgsConstructor
 @RestController
 @RequestMapping("/apartment")
 class ApartmentController {
 
     private ApartmentService apartmentService;
-    private JwtService jwtService;
-
-    ApartmentController(ApartmentService apartmentService, JwtService jwtService) {
-        this.apartmentService = apartmentService;
-        this.jwtService = jwtService;
-    }
 
     @PostMapping
     ResponseEntity<Object> createApartment(@RequestBody Apartment apartment, @Jwt JwtData jwtData) {
-        //JwtData jwtData = jwtService.readToken(token);
         apartmentService.createApartment(apartment.getAddress(), apartment.getCity(), jwtData.getEmail());
         return new ResponseEntity<>(new MessageResponse("Apartment created."), HttpStatus.CREATED);
     }
--- src/main/java/com/piekoszek/nowaksharedrent/invite/InviteController.java
+++ src/main/java/com/piekoszek/nowaksharedrent/invite/InviteController.java
@@ -1,41 +1,34 @@
 package com.piekoszek.nowaksharedrent.invite;
 
+import com.piekoszek.nowaksharedrent.jwt.Jwt;
 import com.piekoszek.nowaksharedrent.jwt.JwtData;
-import com.piekoszek.nowaksharedrent.jwt.JwtService;
 import com.piekoszek.nowaksharedrent.response.MessageResponse;
+import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
+@AllArgsConstructor
 @RestController
 class InviteController {
 
     private InviteService inviteService;
-    private JwtService jwtService;
-
-    InviteController(InviteService inviteService, JwtService jwtService) {
-        this.inviteService = inviteService;
-        this.jwtService = jwtService;
-    }
 
     @PostMapping("/invite")
-    ResponseEntity<Object> createInvitation(@RequestBody Invitation invitation, @RequestHeader("Authorization") String token) {
-        JwtData jwtData = jwtService.readToken(token);
+    ResponseEntity<Object> createInvitation(@RequestBody Invitation invitation, @Jwt JwtData jwtData) {
         inviteService.createInvitation(jwtData.getEmail(), invitation.getReceiver(), invitation.getApartmentId());
         return new ResponseEntity<>(new MessageResponse("Invitation sent."), HttpStatus.OK);
     }
 
     @GetMapping("/invitation")
-    ResponseEntity<Object> getInvitations(@RequestHeader("Authorization") String token) {
-        JwtData jwtData = jwtService.readToken(token);
+    ResponseEntity<Object> getInvitations(@Jwt JwtData jwtData) {
         return new ResponseEntity<>(inviteService.getInvitations(jwtData.getEmail()), HttpStatus.OK);
     }
 
     @PostMapping("/invitation/{id}/accept")
-    ResponseEntity<Object> acceptInvitation(@PathVariable("id") String id, @RequestHeader("Authorization") String token) {
-        JwtData jwtData = jwtService.readToken(token);
+    ResponseEntity<Object> acceptInvitation(@PathVariable("id") String id, @Jwt JwtData jwtData) {
         Optional<String> result = inviteService.resolveInvitation(jwtData.getEmail(), id, true);
         if (result.isPresent()) {
             return new ResponseEntity<>(new MessageResponse(result.get()), HttpStatus.OK);
@@ -44,8 +37,7 @@ class InviteController {
     }
 
     @PostMapping("/invitation/{id}/reject")
-    ResponseEntity<Object> rejectInvitation(@PathVariable("id") String id, @RequestHeader("Authorization") String token) {
-        JwtData jwtData = jwtService.readToken(token);
+    ResponseEntity<Object> rejectInvitation(@PathVariable("id") String id, @Jwt JwtData jwtData) {
         Optional<String> result = inviteService.resolveInvitation(jwtData.getEmail(), id, false);
         if (result.isPresent()) {
             return new ResponseEntity<>(new MessageResponse(result.get()), HttpStatus.OK);
