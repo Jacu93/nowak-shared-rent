66a4cce9ec677ed7ee873009d63ee458c27ea7c3
Jacek Nowak <jackie.n93@gmail.com>
2019-06-20 12:00:01 +0200

    Removing constructor from JwtData. Logging undefined exceptions to console.

--- src/main/java/com/piekoszek/nowaksharedrent/config/UndefinedExceptionHandlerController.java
+++ src/main/java/com/piekoszek/nowaksharedrent/config/UndefinedExceptionHandlerController.java
@@ -1,12 +1,20 @@
 package com.piekoszek.nowaksharedrent.config;
 
+import com.piekoszek.nowaksharedrent.response.MessageResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 
+@Slf4j
 @ControllerAdvice
 class UndefinedExceptionHandlerController {
 
-//    @ExceptionHandler(Throwable.class)
-//    public ResponseEntity<Object> handleThrowableException (Throwable ex, WebRequest request) {
-//        return new ResponseEntity<>(new MessageResponse("Undefined error! Sorry, we are working on that"), HttpStatus.BAD_REQUEST);
-//    }
+    @ExceptionHandler(Throwable.class)
+    public ResponseEntity<Object> handleThrowableException (Throwable ex, WebRequest request) {
+        log.error("Undefined exception: ",ex);
+        return new ResponseEntity<>(new MessageResponse("Undefined error! Sorry, we are working on that"), HttpStatus.BAD_REQUEST);
+    }
 }
