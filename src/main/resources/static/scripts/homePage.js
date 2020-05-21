952b305b7f0f0f303fbf61ea176c98289a064712
Jacek Nowak <jackie.n93@gmail.com>
2020-05-22 00:57:07 +0200

    Correcting homePage.js function loadRoommates. Checking last position of array was wrong.

--- src/main/resources/static/scripts/homePage.js
+++ src/main/resources/static/scripts/homePage.js
@@ -107,7 +107,7 @@ function loadRoommates(ApartmentId) {
             let borderDate = new Date();
             let rentsArrayLength = rentsArray.length;
             tenantsArrayLength = tenantsArray.length;
-            for(i = rentsArrayLength ; i >=0 ; i--) {  
+            for(i = rentsArrayLength-1 ; i >=0 ; i--) {  
 
                 borderDate = new Date(rentsArray[i].borderDate);
                 if (borderDate.getTime() <= currentDate.getTime()) {
